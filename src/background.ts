a343b555573f3ac59153b038c53e772ca84c0e2b
Yamada <coder.yamada.taro@gmail.com>
2023-03-12 16:06:10 +0900

    軽微なバグを修正

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,12 @@
 console.log("background script loaded");
-
+const API_KEY = "YOUR_API_KEY";
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "query") {
     fetch(`https://api.openai.com/v1/engines/davinci-codex/completions?prompt=${request.query}&max_tokens=1024&n=1&stop=%5B%22%5Cn%22%5D`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.API_KEY}`
+        "Authorization": `Bearer ${API_KEY}`
       }
     })
     .then(response => response.json())
